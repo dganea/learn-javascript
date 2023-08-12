c6df1c221a8a061602eeec254f1f54aa7526e37b
Daniel Ganea <dganeaca@gmail.com>
2023-08-12 10:27:14 -0400

    declaration of variables

---
+++ Language_Core.js
@@ -0,0 +1,11 @@
+// *** Defining variables *** //
+
+// Declaration of Variables Using the var Keyword
+var firstName;
+var lastName;
+
+// Declaration of Variables Using the var Keyword (RECOMMENDED)
+let anotherFirstName;
+let anotherLastName;
+
+// Variable initialization
